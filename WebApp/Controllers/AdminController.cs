d46664aa47b7f8f4379b89668d1d36f07ba89fa7
zerdan geyik <zerdan4995@gmail.com>
2025-03-25 15:28:18 +0100

    Add project files.

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        [Route("members")]
+        public IActionResult Members()
+        {
+            // Sätt titeln dynamiskt för denna vy
+            ViewData["Title"] = "Team Members";
+            return View();
+        }
+    }
+}
---
+++ WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[Route("projects")]
+public class ProjectsController : Controller
+{
+
+    [Route("")]
+    public IActionResult Projects ()
+    {
+        return View();
+    }
+}
